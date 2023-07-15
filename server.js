ecb0422d90b98c2f1e014d0dfbf963669b7b15f4
ryland <ryland@MacBook-Pro-4.local>
2023-07-15 12:01:53 -0700

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 app.get('/greeting', (req, res)=>{
     res.send('Hello, stranger')
@@ -13,6 +14,13 @@ app.get('/greeting/:name', (req, res)=>{
 app.get('/tip/:total/:tipPercentage', (req, res)=>{
     console.log(req.params);
     res.send(`If the bill total is $${req.params.total}, and you want to tip %${req.params.tipPercentage}, then you need to tip $${req.params.total * (req.params.tipPercentage/100)}.`)
+})
+
+app.get('/magic/:question', (req, res)=>{
+    let randomAnswer = answers[Math.floor(Math.random()*answers.length)]
+    res.send(req.params.question+'?'+'<h2>Answer:</h2>'+`<h1>${randomAnswer}</h1>`)
+    // res.send(randomAnswer)
+    console.log(req.params);
 })
 
 app.listen(3000, () => {
