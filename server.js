a5f74f74a7e96d036a4d88c69596ab59081e656d
ryland <ryland@MacBook-Pro-4.local>
2023-07-15 11:47:43 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.get('/greeting/:name', (req, res)=>{
     res.send(`What's up, ${stranger}`)
 })
 
-
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    console.log(req.params);
+    res.send(`If the bill total is $${req.params.total}, and you want to tip %${req.params.tipPercentage}, then you need to tip $${req.params.total * (req.params.tipPercentage/100)}.`)
+})
 
 app.listen(3000, () => {
     console.log('listening');
